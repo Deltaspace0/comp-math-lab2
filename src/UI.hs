b59ecc82e72e025ab2f780eb68a8efa5c66c92d7
deltaspace0 <rgadeew@yandex.ru>
2023-05-05 14:41:10 +0300

    show f(x)

--- src/UI.hs
+++ src/UI.hs
@@ -19,29 +19,32 @@ buildUI _ model = tree where
             , labeledCheckbox "Lock Y" yLock
             , separatorLine
             , dropdown calcMethod methods methodTitle methodTitle
-            , dropdown currentEquation [0..length equations-1] f f
+            , dropdown currentEquation [0..length equations-1] et et
             , separatorLine
             , label $ "a = " <> showt a
             , hslider_ pointA (-10) 10 [dragRate 0.1]
             , label $ "b = " <> showt b
             , hslider_ pointB (-10) 10 [dragRate 0.1]
-            , label $ "x = " <> showt (model ^. pointRoot)
+            , label $ "x = " <> showt x
             , hslider_ pointRoot a b [dragRate 0.05]
+            , label $ "f(x) = " <> showt (f x)
             , separatorLine
             , button "Compute next values" AppCompute
             ]
         ] `styleBasic` [padding 16]
     a = model ^. pointA
     b = model ^. pointB
+    x = model ^. pointRoot
+    f = snd . (fst $ equations!!(model ^. currentEquation))
     plot = graph_ (getPoints model)
         [ lockX_ $ model ^. xLock
         , lockY_ $ model ^. yLock
         ]
     methods = [Chord, Newton, Bisection, Iteration, IterationSystem]
-    methodTitle x = label $ case x of
+    methodTitle t = label $ case t of
         Chord -> "Chord method"
         Newton -> "Newton method"
         Bisection -> "Bisection method"
         Iteration -> "Iteration method"
         IterationSystem -> "Iteration method for systems"
-    f x = label $ snd $ equations!!x
+    et t = label $ snd $ equations!!t
